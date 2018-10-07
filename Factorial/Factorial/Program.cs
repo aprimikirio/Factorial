c827fa9a0639235fb168189485a99f535c82dddf
APrimikirio <a.d.primikirio@gmail.com>
2018-10-07 21:32:12 +0300

    add a working version of the inverse factorial method

--- Factorial/Factorial/Program.cs
+++ Factorial/Factorial/Program.cs
@@ -1,15 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Factorial
 {
     class Program
     {
-        static void Main(string[] args)
+        static int ReFactorial(int n)
         {
+            int i = 1;
+
+            while (n > i)
+            {
+                if (n % i != 0)
+                    return -1;
+                n /= i;
+                ++i;
+            }
+
+            if (i == n)
+                return i;
+            return -1;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(ReFactorial(5040));
+            Console.ReadKey();
         }
     }
 }
